2cf5c8aae07ffca349745a74dc5f29f02e9d297a
Tomás Sasovsky <to.sasovsky@gmail.com>
2023-06-01 16:21:05 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.ai_recording_visualizer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:ai_recording_visualizer/app/app.dart';
+import 'package:ai_recording_visualizer/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
